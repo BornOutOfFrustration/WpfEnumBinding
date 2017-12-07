954ed2421412977fcd5405d656da30da4bf21937
gjansen <geert__jansen@hotmail.com>
2017-12-07 16:37:21 +0100

    ExampleCode

---
+++ WpfEnumBindingExample/WpfEnumBindingExample/EnumToDisplayAttributeConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfEnumBindingExample {
+    public class EnumToDisplayAttributeConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value == null) {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (!value.GetType().IsEnum) {
+                throw new ArgumentException("Value must be an enumeration type");
+            }
+
+            var fieldInfo = value.GetType().GetField(value.ToString());
+            var array = fieldInfo.GetCustomAttributes(false);
+
+            foreach(var attrib in array) {
+                if(attrib is DisplayAttribute) {
+                    var displayAttrib = attrib as DisplayAttribute;
+
+                    if(displayAttrib.ResourceType == null) { return displayAttrib.Name; }
+
+                    ResourceManager resourceManager = new ResourceManager(
+                        displayAttrib.ResourceType.FullName,
+                        displayAttrib.ResourceType.Assembly);
+                    var entry = resourceManager.GetResourceSet(
+                        System.Threading.Thread.CurrentThread.CurrentCulture, true, true)
+                        .OfType<DictionaryEntry>()
+                        .FirstOrDefault(p => p.Key.ToString() == displayAttrib.Name);
+
+                    var key = entry.Value.ToString();
+                    return key;
+                }
+            }
+
+            string name = Enum.GetName(value.GetType(), value);
+            return name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
---
+++ WpfEnumBindingExample/WpfEnumBindingExample/Shirt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEnumBindingExample {
+    public enum ShirtType {
+        [Display(Name ="Dress Shirt")]
+        DressShirt,
+        [Display(Name ="T-Shirt")]
+        TShirt,
+        CampShirt,
+        Polo,
+        Jersey,
+    }
+
+    public class Shirt : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ShirtType stype = ShirtType.DressShirt;
+        public ShirtType SType {
+            get { return this.stype; }
+            set { SetProperty(ref this.stype, value); }
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null) {
+            if(Object.Equals(storage, value)) { return false; }
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged(string propertyName) {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
